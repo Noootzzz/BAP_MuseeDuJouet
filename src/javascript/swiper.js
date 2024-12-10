e80f1ba22e210e216632e58cc0b944ee5991ace1
Ilyan-Jude Bain-Trimbach <ilyanjudecour@gmail.com>
2024-12-10 18:51:40 +0100

    choix de la couleur de la voiture

--- src/javascript/swiper.js
+++ src/javascript/swiper.js
@@ -17,37 +17,121 @@ function updateHighlightedSlide(swiperInstance) {
   index_active = swiperInstance.slides[activeIndex].getAttribute(
     "data-swiper-slide-index"
   );
-  document.getElementById("couleur_input").value = index_active;
-  console.log(index_active);
 
   const highlightedSlide = document.getElementById("highlighted-slide");
 
-  const centerSlideImages = centerSlide.querySelectorAll("img");
+  const baseBackImage = centerSlide.querySelector("#Base_Back");
+  const baseFrontImage = centerSlide.querySelector("#Base_Front");
 
-  if (centerSlideImages.length > 0) {
-    highlightedSlide.innerHTML = "";
-
-    centerSlideImages.forEach((image) => {
-      const newImage = document.createElement("img");
-      newImage.src = image.src;
-      newImage.alt = image.alt;
-      newImage.classList.add(
-        "absolute",
-        "top-1/2",
-        "left-1/2",
-        "-translate-x-1/2",
-        "-translate-y-1/2",
-        "scale-150"
-      );
-
-      highlightedSlide.appendChild(newImage);
-    });
-
-    const computedStyle = getComputedStyle(centerSlide);
-    const backgroundColor = computedStyle.backgroundColor;
-
-    if (backgroundColor) {
-      highlightedSlide.style.backgroundColor = backgroundColor;
-    }
+  if (baseBackImage && baseFrontImage) {
+    highlightedSlide.querySelector("#Base_Back").src = baseBackImage.src;
   }
+
+  if (baseFrontImage) {
+    highlightedSlide.querySelector("#Base_Front").src = baseFrontImage.src;
+  }
+  const bgColor = getComputedStyle(centerSlide).backgroundColor;
+  highlightedSlide.style.backgroundColor = bgColor;
+
+  return index_active;
 }
+
+const couleur_tab_E = ["blue", "yellow", "pink", "red", "green"];
+const couleur_tab = ["Bleue", "Jaune", "Rose", "Rouge", "Vert"];
+
+const crea_slide = (annee = "50's") => {
+  const container = document.getElementById("swiper-wrapper");
+  const template = document.getElementById("swiper-slide-template");
+
+  let i = 0;
+  container.innerHTML = "";
+  couleur_tab.forEach((couleur) => {
+    const VoitureCard = template.cloneNode(true);
+    VoitureCard.style.display = "block";
+    VoitureCard.querySelector(
+      "#Base_Back"
+    ).src = `./src/img/${annee}/Base_Back/Low-50\'s-Base-Si\ège-${couleur}.webp`;
+
+    VoitureCard.querySelector(
+      "#Base_Front"
+    ).src = `./src/img/${annee}/Base_Front/Low-50\'s-Base-${couleur}.webp`;
+
+    VoitureCard.classList.add(`bg-${couleur_tab_E[i]}-500`);
+    VoitureCard.classList.add("swiper-slide");
+    i++;
+    container.appendChild(VoitureCard);
+  });
+};
+
+const crea_template = (
+  annee = "50's",
+  volant = "01",
+  motif = undefined,
+  jante = "01",
+  couleur = "bleue",
+  aileron = undefined,
+  couleur_aileron = undefined
+) => {
+  document.getElementById("swiper-slide-template").innerHTML = `
+          <img
+            id="pneus_gauche"
+            src="./src/img/${annee}/Pneu/Low-50's-Pneus-Gauche.webp"
+            alt="Image pneus gauche"
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            id="Base_Back"
+            src="./src/img/${annee}/Base_Back/Low-50's-Base-Siège-${couleur}.webp"
+            alt="Image Base Back"
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            id="volant"
+            src="./src/img/${annee}/Volant/Low-50's-Volant-${volant}.webp"
+            alt="Image Volant"
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            id="Base_Front"
+            src="./src/img/${annee}/Base_Front/Low-50's-Base-${couleur}.webp"
+            alt="Image Base Front"
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            id="pneus_droit"
+            src="./src/img/${annee}/Pneu/Low-50's-Pneus-Droit.webp"
+            alt="Image Pneus Droit"
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+          <img
+            id="jante"
+            src="./src/img/${annee}/Jantes/Low-50's-Jantes-${jante}.webp"
+            alt="Image Jante"
+            class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>`;
+  if (motif) {
+    document.getElementById("swiper-slide-template").innerHTML += `
+      <img
+        id="motif"
+        src="./src/img/${annee}/Motif\Low-50's-Motif-${motif}.webp"
+        alt="Image Jante"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />`;
+  }
+  if (aileron && couleur_aileron) {
+    document.getElementById("swiper-slide-template").innerHTML += `      
+      <img
+        id="aileron_front"
+        src="./src/img/${annee}/Aileron_Front\Low-50's-Aileron-Front-${aileron}-${couleur_aileron}.webp"
+        alt="Image Jante"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />
+      <img
+        id="aileron_back"
+        src="./src/img/${annee}/Aileron_Back\Low-50's-Aileron-Back-${aileron}-${couleur_aileron}.webp"
+        alt="Image Jante"
+        class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+      />`;
+  }
+};
