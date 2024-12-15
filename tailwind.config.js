80871ca876a4b1e9128d2bdbdf1e86506d5bac7c
Ilyan-Jude Bain-Trimbach <ilyanjudecour@gmail.com>
2024-12-15 02:22:59 +0100

    petite correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
+    "./index.html",
     "./voiture.html",
     "./personnalisation.html",
+    "./perso_aileron.html",
+    "./perso_fond.html",
+    "./name.html",
+    "./qr.html",
+    "./galerie.html",
     "./src/**/*.{html,js}",
   ],
   theme: {
